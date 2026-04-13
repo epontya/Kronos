bfa6436474110a6ae69d472217b9170fb46781f8
Epon Tya <epontya@users.noreply.github.com>
2026-04-13 10:50:37 +0800

    Add TimeSeriesDataset to top-level imports for convenience

--- kronos/__init__.py
+++ kronos/__init__.py
@@ -5,11 +5,13 @@ Fork of shiyu-coder/Kronos with additional features and improvements.
 Personal fork notes:
 - Using this for experimenting with stock price prediction
 - See /notebooks for example usage
+- Added convenience import for TimeSeriesDataset
 """
 
 from kronos.model import Kronos
 from kronos.predictor import KronosPredictor
 from kronos.data_utils import prepare_data, normalize_data, denormalize_data
+from kronos.dataset import TimeSeriesDataset
 
 __version__ = "0.2.0"
 __author__ = "Kronos Contributors"
@@ -21,4 +23,5 @@ __all__ = [
     "prepare_data",
     "normalize_data",
     "denormalize_data",
+    "TimeSeriesDataset",
 ]
